a86e41e0f89ce1c682e3184d0596709d2301fedf
Kinnslayer <rodrigo.s.alencar@hotmail.com>
2023-01-26 02:52:35 -0300

    listagem de cursos com API HTTP GET

--- crud-spring/src/main/java/com/rodrigo/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/rodrigo/CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package com.rodrigo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.rodrigo.model.Course;
+import com.rodrigo.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +15,17 @@ public class CrudSpringApplication {
         SpringApplication.run(CrudSpringApplication.class, args);
     }
     
+    @Bean
+    CommandLineRunner initDatabase(CourseRepository courseRepository){
+        return args -> { 
+            courseRepository.deleteAll();
+
+            Course c = new Course();
+            c.setName("Angular com Spring");
+            c.setCategory("front-end");
+
+            courseRepository.save(c);
+        };
+    }
+
 }
--- crud-spring/src/main/java/com/rodrigo/controller/CourseController.java
+++ crud-spring/src/main/java/com/rodrigo/controller/CourseController.java
@@ -7,14 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rodrigo.model.Course;
+import com.rodrigo.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/api/courses")
+@AllArgsConstructor
 public class CourseController {
     
+    private final CourseRepository courseRepository;
+
     //@RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public List<Course> list() {
-        return null;
+        return courseRepository.findAll();
     }
 }
